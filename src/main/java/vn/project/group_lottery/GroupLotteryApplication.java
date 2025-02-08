bb141b7b60ff9ddf66d5805ba24885efb79e9d9b
NhatThang19 <lanhatthang1910@gmail.com>
2025-02-08 14:28:20 +0700

    first commit

---
+++ src/main/java/vn/project/group_lottery/GroupLotteryApplication.java
@@ -0,0 +1,13 @@
+package vn.project.group_lottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupLotteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupLotteryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/project/group_lottery/GroupLotteryApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.project.group_lottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupLotteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
